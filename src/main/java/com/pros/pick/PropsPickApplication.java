099b4744ab6826a015b538603cd8b498db340e1c
yunyoung1819 <yunyoung1819@hanmail.net>
2023-08-19 03:50:41 +0900

    project init

---
+++ src/main/java/com/pros/pick/PropsPickApplication.java
@@ -0,0 +1,13 @@
+package com.pros.pick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropsPickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropsPickApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pros/pick/PropsPickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pros.pick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropsPickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
